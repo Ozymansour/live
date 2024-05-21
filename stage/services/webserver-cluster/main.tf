421bdb0085acb952d44df80c8dbb8e929b69326c
Ahmed Mansour <ahmedsmansour@proton.me>
2024-05-21 12:27:07 +0300

    Updated module release version

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "https://github.com/Ozymansour/modules/tree/main/services/webserver-cluster?ref=0.0.1"
+  source = "github.com/Ozymansour/modules//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name = "webservers-stage"
   db_remote_state_bucket = "terraform-state-134679"
